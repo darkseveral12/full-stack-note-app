ecfdd1f8f957cddb624efb50c98c858f0ff1f12d
aizensosuke <clevencastillo10@gmail.com>
2026-04-17 13:28:51 +0800

    again

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://full-stack-note-app-fi75.vercel.app",
+      "https://full-stack-note-app-fi75-l6yvcinr3-darkseveral12s-projects.vercel.app",
     ],
     credentials: true,
   }),
