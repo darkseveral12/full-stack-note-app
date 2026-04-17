8f4e2c38163ba8c0698fb6b6e4d7f3601d6ca953
aizensosuke <clevencastillo10@gmail.com>
2026-04-17 12:53:06 +0800

    added new url cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ const mongoDBURL = process.env.DB_URL;
 console.log(mongoDBURL);
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin:[ "http://localhost:5173", "https://full-stack-note-app-crlx.vercel.app"] , credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
